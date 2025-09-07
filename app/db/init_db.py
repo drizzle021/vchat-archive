bd2efc6f272de62166e7411df572c4ce654ad535
drizzle021 <gabrielabraham004@gmail.com>
2025-09-07 23:10:19 +0200

    updated init script after table partitioning

--- app/db/init_db.py
+++ app/db/init_db.py
@@ -1,10 +1,11 @@
 from app.db.base import Base
 from app.db.session import engine
 from app.models.video import Video
-from app.models.message import Message
+from app.models.commenters import Commenters
 
 def init_db():
-    Base.metadata.create_all(bind=engine)
+    tables_to_create = [Video.__table__, Commenters]  # Add other non-partitioned models here
+    Base.metadata.create_all(bind=engine, tables=tables_to_create)
 
 if __name__ == "__main__":
     init_db()
