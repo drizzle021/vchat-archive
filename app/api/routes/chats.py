dee7d1f0ce8d1286e6055ba8c8445e1bdc616821
drizzle021 <gabrielabraham004@gmail.com>
2025-09-07 23:08:27 +0200

    added error messages

--- app/api/routes/chats.py
+++ app/api/routes/chats.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter, Query
 from app.db.session import SessionLocal
 from app.models.message import Message
+from app.models.video import Video
+from fastapi import HTTPException
 
 router = APIRouter()
 
@@ -13,6 +15,12 @@ def get_chats(
     db = SessionLocal()
 
     total = db.query(Message).filter_by(video_id=video_id).count()
+
+    if total == 0:
+        video = db.query(Video).filter_by(video_id=video_id).first()
+        if not video:
+            raise HTTPException(status_code=404, detail=f"Video '{video_id}' does not exist.")
+        raise HTTPException(status_code=404, detail="No messages found for this video.")
 
     messages = (
         db.query(Message)
@@ -32,6 +40,7 @@ def get_chats(
         "offset": offset,
         "messages": [
             {
+                "id":m.id,
                 "author": m.author,
                 "message": m.message,
                 "timestamp": m.timestamp
