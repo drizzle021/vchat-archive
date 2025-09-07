6a537f7ee6de882c911fb9a846a63ec55a4d81a3
drizzle021 <gabrielabraham004@gmail.com>
2025-09-07 23:07:28 +0200

    allowed CORS

--- app/api/main.py
+++ app/api/main.py
@@ -1,5 +1,15 @@
 from fastapi import FastAPI
 from app.api.router import router
+from fastapi.middleware.cors import CORSMiddleware
+
+
 
 app = FastAPI()
 app.include_router(router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
