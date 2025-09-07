e0b5c856bfb4d181247da92562b3d0c63713fd0b
drizzle021 <gabrielabraham004@gmail.com>
2025-09-07 23:08:57 +0200

    added some filters

--- app/api/routes/videos.py
+++ app/api/routes/videos.py
@@ -1,6 +1,9 @@
 from fastapi import APIRouter, Query
 from app.db.session import SessionLocal
 from app.models.video import Video
+from app.models.commenters import Commenters
+from app.utils.normalize import normalize
+
 router = APIRouter()
 
 @router.get("/videos")
@@ -15,3 +18,25 @@ def get_videos(channel: str = Query(default=None)):
     db.close()
 
     return [{"id": v.video_id, "title": v.title, "channel": v.channel} for v in videos]
+
+@router.get("/videos/by-author")
+def get_videos_by_author(author: str = Query(...)):
+    db = SessionLocal()
+
+    video_ids = db.query(Commenters.video_id).filter(Commenters.author.ilike(f"%{author}%")).all()
+    video_ids = [vid[0] for vid in video_ids]
+
+    videos = db.query(Video).filter(Video.video_id.in_(video_ids)).all()
+    return videos
+
+
+@router.get("/videos/by-title")
+def get_videos_by_author(q: str = Query(...)):
+    db = SessionLocal()
+
+    q = normalize(q)
+    
+    pattern = f"%{q.lower()}%"
+
+    return db.query(Video).filter(Video.title.ilike(pattern)).all()
+
