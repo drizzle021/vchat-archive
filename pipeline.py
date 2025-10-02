9ff9f364155270f36c76a08e22e7c1c5e9437b23
drizzle021 <gabrielabraham004@gmail.com>
2025-10-02 16:21:11 +0200

    refactored

--- pipeline.py
+++ pipeline.py
@@ -1,9 +1,12 @@
 from scripts.fetch_all_vods import fetch_all_vods
 from scripts.download_chats import download_all_chats
 from scripts.import_chats import run_import
+from app.utils.logging import logger
 
 def run_pipeline():
-    fetch_all_vods(20)
+    VODS_PER_CHANNEL = 120
+    logger.info(f"Starting pipeline with {VODS_PER_CHANNEL} VODS/CHANNEL")
+    fetch_all_vods(VODS_PER_CHANNEL)
     download_all_chats()
     run_import()
 
